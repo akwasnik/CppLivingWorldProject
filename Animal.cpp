de610c7451d1796e98fd0fdf42ca2081f2f0cf8b
akwasnik <adrian.kwasnik@gmail.com>
2025-04-11 20:30:29 +0200

    template files added

---
+++ Animal.cpp
@@ -0,0 +1,11 @@
+#include "Animal.h"
+
+Animal::Animal(int power, Position position) : Organism(power, position)
+{
+	setSpecies("A");
+}
+
+Animal::Animal() : Organism()
+{
+	setSpecies("A");
+}
---
+++ Plant.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "Organism.h"
+
+class Plant : public Organism
+{
+	public:
+		Plant(int power, Position position);
+		Plant();
+
+		void move(int dx, int dy);
+};
